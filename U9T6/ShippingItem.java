4885d24b484ac409e3d4737e8acc67a3dbe21e5f
Chen Zhi Lin <chenzhil@nycstudents.net>
2025-03-25 12:43:16 +0000

    a

---
+++ U9T6/ShippingItem.java
@@ -0,0 +1,19 @@
+package U9T6;
+
+public class ShippingItem {
+    private double weight; // in ounces
+    private static double COST_PER_OZ = 1.5;
+
+    public ShippingItem(double w) {
+        weight = w;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getCost() {
+        /* TO BE IMPLEMENTED IN PART A */
+        return COST_PER_OZ * getWeight();
+    }
+}
